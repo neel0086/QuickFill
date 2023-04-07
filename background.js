89a121c6705018d46955ae1aa5bcbfaacc5b44d8
Neel Mehta <neelmehta0086@gmail.com>
2023-04-07 18:56:09 +0530

    feat: dropdown for suggestion

--- background.js
+++ background.js
@@ -7,78 +7,14 @@ chrome.storage.onChanged.addListener(function (changes, namespace) {
                 data = result.myData
         })
 });
-// chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-//         if (changeInfo.status === 'complete') {
-//                 chrome.storage.local.get('myData', function (result) {
-//                         data = result.myData
-
-
-//                         // Retrieve the key-value pairs from Chrome storage
-//                         var suggestions = [];
-//                         for (var key in data) {
-//                                 suggestions.push(key);
-//                         }
-//                         console.log(suggestions)
-//                         if (document.readyState !== "loading") {
-//                                 // Find all input fields on the page and add an event listener to each one
-//                                 var inputFields = document.querySelectorAll('input[type="text"], input[type="search"]');
-//                                 console.log(inputFields)
-//                                 inputFields.forEach(function (input) {
-//                                         input.addEventListener('input', function () {
-//                                                 var currentVal = input.value.toLowerCase();
-//                                                 var matchedSuggestions = [];
-//                                                 console.log("inside input")
-//                                                 // Find the suggestions that match the current value
-//                                                 for (var i = 0; i < suggestions.length; i++) {
-//                                                         if (suggestions[i].toLowerCase().indexOf(currentVal) === 0) {
-//                                                                 matchedSuggestions.push(suggestions[i]);
-//                                                         }
-//                                                 }
-
-//                                                 // Display the suggestions as a dropdown
-//                                                 var dropdown = document.createElement('div');
-//                                                 dropdown.classList.add('suggestion-dropdown');
-//                                                 matchedSuggestions.forEach(function (suggestion) {
-//                                                         var suggestionItem = document.createElement('div');
-//                                                         suggestionItem.classList.add('suggestion-item');
-//                                                         suggestionItem.textContent = suggestion;
-//                                                         dropdown.appendChild(suggestionItem);
-//                                                 });
-//                                                 input.parentNode.appendChild(dropdown);
-//                                         });
-//                                 });
-
-//                                 // Hide the suggestion dropdown when the user clicks outside of it
-//                                 document.addEventListener('click', function (event) {
-//                                         var dropdowns = document.querySelectorAll('.suggestion-dropdown');
-//                                         dropdowns.forEach(function (dropdown) {
-//                                                 if (!dropdown.contains(event.target)) {
-//                                                         dropdown.parentNode.removeChild(dropdown);
-//                                                 }
-//                                         });
-//                                 });
-//                         }
-
-//                 })
-
-//         }
-// })
 
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
         if (changeInfo.status === 'complete') {
                 chrome.storage.local.get('myData', function (result) {
+                        console.log("Hello from backgroundd")
                         var data = result.myData;
-
-                        // Retrieve the key-value pairs from Chrome storage
-                        var suggestions = [];
-                        for (var key in data) {
-                                suggestions.push(key);
-                        }
-
-                        // Inject a content script to handle the suggestion dropdown logic
                         chrome.tabs.executeScript(tabId, { file: 'content.js' }, function () {
-                                // Pass the suggestions to the content script
-                                chrome.tabs.sendMessage(tabId, { suggestions: suggestions });
+                                chrome.tabs.sendMessage(tabId, { suggestions: data },{ frameId: 0 });
                         });
                 });
         }
--- content.js
+++ content.js
@@ -1,46 +1,107 @@
-// content.js
+var executed = false;
 
-// Listen for messages from the background page
-chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    console.log("hello")
-    // if (message.type === 'suggestions') {
-    console.log("hello1")
+if (!executed) {
+    var dropdownIndex = -1;
+    executed=true
+    console.log(chrome.runtime.getManifest().name)
+    chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+        var inputFields = document.querySelectorAll('input[type="text"], input[type="search"]');
+        inputFields.forEach(function (input) {
+            input.addEventListener('input', function () {
+                input.setAttribute("autocomplete", "off");
+                var searchKey = input.value.toLowerCase();
+                var temp = [];
 
-    var inputFields = document.querySelectorAll('input[type="text"], input[type="search"]');
-    console.log(inputFields)
-    inputFields.forEach(function (input) {
-        input.addEventListener('input', function () {
-            var currentVal = input.value.toLowerCase();
-            var matchedSuggestions = [];
-
-            // Find the suggestions that match the current value
-            for (var i = 0; i < message.suggestions.length; i++) {
-                if (message.suggestions[i].toLowerCase().indexOf(currentVal) === 0) {
-                    matchedSuggestions.push(message.suggestions[i]);
+                // Find the suggestions that match the current value
+                var key = message.suggestions;
+                for (const subKey in key) {
+                    if (String(key[subKey]).toLowerCase().includes(searchKey)) {
+                        temp.push(key[subKey]);
+                    }
                 }
-            }
 
-            // Display the suggestions as a dropdown
-            var dropdown = document.createElement('div');
-            dropdown.classList.add('suggestion-dropdown');
-            matchedSuggestions.forEach(function (suggestion) {
-                var suggestionItem = document.createElement('div');
-                suggestionItem.classList.add('suggestion-item');
-                suggestionItem.textContent = suggestion;
-                dropdown.appendChild(suggestionItem);
+                // Remove previously appended dropdown (if any)
+                var prevDropdown = input.parentNode.querySelector('.suggestion-dropdown');
+                if (prevDropdown) {
+                    prevDropdown.remove();
+                }
+
+                // Display the suggestions as a dropdown
+                var dropdown = document.createElement('div');
+                dropdown.classList.add('suggestion-dropdown');
+                temp.forEach(function (suggestion) {
+                    var suggestionItem = document.createElement('div');
+                    suggestionItem.classList.add('suggestion-item');
+                    suggestionItem.textContent = suggestion;
+                    dropdown.appendChild(suggestionItem);
+
+                    suggestionItem.addEventListener('click', function () {
+                        input.value = suggestion
+                        dropdown.parentNode.removeChild(dropdown)
+                    })
+                });
+                input.parentNode.appendChild(dropdown);
             });
-            input.parentNode.appendChild(dropdown);
-        });
-    });
+            input.addEventListener('keyup', function (event) {
+                var dropdown = input.parentNode.querySelector('.suggestion-dropdown');
+                if (dropdown) {
+                    var suggestionItems = dropdown.querySelectorAll('.suggestion-item');
+                    if (event.keyCode === 40) { // down arrow
+                        event.preventDefault();
+                        if(dropdownIndex>=0 && dropdownIndex<suggestionItems.length && suggestionItems[dropdownIndex].classList.contains("selected")){
+                            suggestionItems[dropdownIndex].classList.remove('selected');
+                        }
+                        dropdownIndex++;
+                        console.log(suggestionItems, dropdownIndex)
+                        if (dropdownIndex < suggestionItems.length) {
+                            suggestionItems[dropdownIndex].classList.add('selected');
+                            input.value = suggestionItems[dropdownIndex].textContent;
+                        }
+                        else {
+                            dropdownIndex = 0;
+                            suggestionItems[dropdownIndex].classList.add('selected');
+                            input.value = suggestionItems[dropdownIndex].textContent;
+                        }
+                    } else if (event.keyCode === 38) { // up arrow
+                        event.preventDefault();
+                        if(dropdownIndex>=0 && dropdownIndex<suggestionItems.length && suggestionItems[dropdownIndex].classList.contains("selected")){
+                            suggestionItems[dropdownIndex].classList.remove('selected');
+                        }
+                        dropdownIndex--;
+                        if (dropdownIndex >= 0) {
+                            suggestionItems[dropdownIndex].classList.add('selected');
+                            input.value = suggestionItems[dropdownIndex].textContent;
+                        } 
+                        else{
+                            console.log(dropdownIndex)
+                            dropdownIndex = suggestionItems.length - 1
+                            suggestionItems[dropdownIndex].classList.add('selected');
+                            input.value = suggestionItems[dropdownIndex].textContent;
+                        }
+                    } else if (event.keyCode === 13) { // enter key
+                        event.stopPropagation()
+                        event.preventDefault();
+                        if (dropdownIndex >= 0) {
+                            input.value = suggestionItems[dropdownIndex].textContent;
+                            dropdown.parentNode.removeChild(dropdown);
+                        }
+                    }
+                }
+            });
 
-    // Hide the suggestion dropdown when the user clicks outside of it
-    document.addEventListener('click', function (event) {
-        var dropdowns = document.querySelectorAll('.suggestion-dropdown');
-        dropdowns.forEach(function (dropdown) {
-            if (!dropdown.contains(event.target)) {
-                dropdown.parentNode.removeChild(dropdown);
-            }
+
         });
+
+
+        // Hide the suggestion dropdown when the user clicks outside of it
+        document.addEventListener('click', function (event) {
+            var dropdowns = document.querySelectorAll('.suggestion-dropdown');
+            dropdowns.forEach(function (dropdown) {
+                if (!dropdown.contains(event.target)) {
+                    dropdown.parentNode.removeChild(dropdown);
+                }
+            });
+        });
+        // }
     });
-    // }
-});
+}
